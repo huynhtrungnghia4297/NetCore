ca0f48c29ec72f67bf3af482e0974da30c5c4c3b
NN <NN@PC17>
2020-05-10 10:41:05 +0700

    Ngay 1

---
+++ NetCore/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore.Controllers
+{
+    public class DemoController : Controller
+    {
+        //local:port/Demo/Chuoi
+
+        public string Chuoi()
+        {
+            return "Hello World";
+        }
+        //local:port/Demo/So
+        public int So()
+        {
+            return new Random().Next(1, 100);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
